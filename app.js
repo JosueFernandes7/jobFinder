3df466ec3db2dd3d4acafd5cef21046b3fa4a014
Josué Fernandes <josue7mf@hotmail.com>
2023-06-27 13:49:52 -0300

    :sparkles: feat: HTML da página de adicionar jobs

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ app.listen(PORT, () => {
   console.log("O Express está rodando na porta " + PORT);
 })
 // body parser
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 // db connection
 db.authenticate()
-.then(() => {
-  console.log("Conectou no banco");
-}).catch(err => {
-  console.log(`Ocorreu um erro ${err}`);
-})
+  .then(() => {
+    console.log("Conectou no banco");
+  }).catch(err => {
+    console.log(`Ocorreu um erro ${err}`);
+  })
 // Routes
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
   res.send("Está funcionando")
 })
 
